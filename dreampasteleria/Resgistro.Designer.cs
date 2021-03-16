93f1078b61efe8a152c0540122df4aa27fdf551a
christopheremv <79923784+christopheremv@users.noreply.github.com>
2021-03-15 20:37:08 -0600

    inicio

---
+++ dreampasteleria/Resgistro.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace dreampasteleria
+{
+    partial class ventanaregistro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ventanaregistro));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rnombre = new System.Windows.Forms.TextBox();
+            this.rapellido = new System.Windows.Forms.TextBox();
+            this.rcorre = new System.Windows.Forms.TextBox();
+            this.rcontraseña = new System.Windows.Forms.TextBox();
+            this.rconfirmecontra = new System.Windows.Forms.TextBox();
+            this.registrarme = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rsexo = new System.Windows.Forms.ComboBox();
+            this.AtrasR = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(48, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RESISTRARME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(9, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombres(s)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(9, 64);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido(s)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 120);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(43, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sexo";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 176);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Correo Electronico";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 232);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 21);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Contraseña";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 288);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 21);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Confirme su contraseña";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(326, 528);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 21);
+            this.label8.TabIndex = 7;
+            // 
+            // rnombre
+            // 
+            this.rnombre.Location = new System.Drawing.Point(9, 32);
+            this.rnombre.Name = "rnombre";
+            this.rnombre.Size = new System.Drawing.Size(237, 29);
+            this.rnombre.TabIndex = 8;
+            // 
+            // rapellido
+            // 
+            this.rapellido.Location = new System.Drawing.Point(9, 88);
+            this.rapellido.Name = "rapellido";
+            this.rapellido.Size = new System.Drawing.Size(237, 29);
+            this.rapellido.TabIndex = 9;
+            // 
+            // rcorre
+            // 
+            this.rcorre.Location = new System.Drawing.Point(9, 200);
+            this.rcorre.Name = "rcorre";
+            this.rcorre.Size = new System.Drawing.Size(237, 29);
+            this.rcorre.TabIndex = 11;
+            // 
+            // rcontraseña
+            // 
+            this.rcontraseña.Location = new System.Drawing.Point(12, 256);
+            this.rcontraseña.Name = "rcontraseña";
+            this.rcontraseña.Size = new System.Drawing.Size(234, 29);
+            this.rcontraseña.TabIndex = 12;
+            // 
+            // rconfirmecontra
+            // 
+            this.rconfirmecontra.Location = new System.Drawing.Point(12, 312);
+            this.rconfirmecontra.Name = "rconfirmecontra";
+            this.rconfirmecontra.Size = new System.Drawing.Size(234, 29);
+            this.rconfirmecontra.TabIndex = 13;
+            // 
+            // registrarme
+            // 
+            this.registrarme.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.registrarme.Location = new System.Drawing.Point(74, 497);
+            this.registrarme.Name = "registrarme";
+            this.registrarme.Size = new System.Drawing.Size(189, 52);
+            this.registrarme.TabIndex = 14;
+            this.registrarme.Text = "REGISTRARME";
+            this.registrarme.UseVisualStyleBackColor = true;
+            this.registrarme.Click += new System.EventHandler(this.registrarme_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.rsexo);
+            this.panel1.Controls.Add(this.rcontraseña);
+            this.panel1.Controls.Add(this.rnombre);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.rconfirmecontra);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.rcorre);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.rapellido);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Location = new System.Drawing.Point(39, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(278, 363);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // rsexo
+            // 
+            this.rsexo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.rsexo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rsexo.FormattingEnabled = true;
+            this.rsexo.Location = new System.Drawing.Point(12, 144);
+            this.rsexo.Name = "rsexo";
+            this.rsexo.Size = new System.Drawing.Size(237, 29);
+            this.rsexo.TabIndex = 0;
+            // 
+            // AtrasR
+            // 
+            this.AtrasR.Location = new System.Drawing.Point(3, 2);
+            this.AtrasR.Name = "AtrasR";
+            this.AtrasR.Size = new System.Drawing.Size(79, 33);
+            this.AtrasR.TabIndex = 16;
+            this.AtrasR.Text = "Atras";
+            this.AtrasR.UseVisualStyleBackColor = true;
+            this.AtrasR.Click += new System.EventHandler(this.AtrasR_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // ventanaregistro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 597);
+            this.Controls.Add(this.AtrasR);
+            this.Controls.Add(this.registrarme);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "ventanaregistro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Apellido(s)";
+            this.Load += new System.EventHandler(this.Resgistro_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox rnombre;
+        private System.Windows.Forms.TextBox rapellido;
+        private System.Windows.Forms.TextBox rcorre;
+        private System.Windows.Forms.TextBox rcontraseña;
+        private System.Windows.Forms.TextBox rconfirmecontra;
+        private System.Windows.Forms.Button registrarme;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox rsexo;
+        private System.Windows.Forms.Button AtrasR;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ dreampasteleria/Resgistro.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dreampasteleria
+{
+    public partial class ventanaregistro : Form
+    {
+        string[] sexo = {"Masculino","Femenino"};
+        //string[] paises_disponible;
+
+
+        public ventanaregistro()
+        {
+            InitializeComponent();
+
+          //  string listado_paises = Properties.Resources.paises.ToString();
+           // paises_disponible = listado_paises.Split(new[] { "\r\n" },StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+        private void Resgistro_Load(object sender, EventArgs e)
+        {
+            recorridosexo();
+            //registrarme.Enabled = false;
+  
+        }
+        void recorridosexo() {
+            for (int i=0;i<sexo.Length;i++) {
+                rsexo.Items.Add(sexo[i]);  
+            }
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void registrarme_Click(object sender, EventArgs e)
+        {
+            switch (validacion())
+            {
+                case 0:
+                    {
+                        errorProvider1.SetError(rnombre, "");
+                        errorProvider1.SetError(rapellido, "");
+                        errorProvider1.SetError(rcontraseña, "");
+                        errorProvider1.SetError(rconfirmecontra, "");
+                        errorProvider1.SetError(rcontraseña, "");
+                        errorProvider1.SetError(rsexo, "");
+                        String mensaje = "Se creo la cuenta,\n" + "Se te envió un correo para activar tu cuenta.\n" + "Es necesario activarla para entrar al sitio.";
+                        MessageBoxButtons boton = MessageBoxButtons.OK;
+                        MessageBox.Show(mensaje, "CONFIRMACION", boton);
+
+
+
+                        break;
+                    }
+                     case 1:
+                    {
+                        errorProvider1.SetError(panel1, "Selecione un sexo");
+                        errorProvider1.SetError(panel1, "");
+                        break;
+                    }
+                     case 2:
+                    {
+                        errorProvider1.SetError(panel1, "error en datos personales");
+                        errorProvider1.SetError(panel1, "");
+                        break;
+                    }
+
+
+            }
+
+        }
+        int validacion()
+        {
+            if (rsexo.SelectedIndex <= -1)
+            {
+                return 1;
+            }
+            else
+            {
+                if (rnombre.Text.Trim()!= String.Empty || rapellido.Text.Trim()!= String.Empty||rcontraseña.Text.Trim() != String.Empty||rcontraseña.Text.Trim() != String.Empty||rconfirmecontra.Text.Trim()!= String.Empty)
+                { 
+                return 0;
+                }
+                else
+                {
+                    return 2;
+                }
+
+              // else
+              //  {
+                   // return 0;
+              ///  }
+
+            }
+        }
+
+        private void AtrasR_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    }
+
+
+    
+
